b9845554004f3016e7ba3eed4f337dde1282c324
Dan Morriss <danielmorriss1@gmail.com>
2025-07-29 21:50:36 +0100

    add divider

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { AboutPageComponent } from "../about-page/about-page.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutPageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
