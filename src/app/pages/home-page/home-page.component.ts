eb3f3f514f83c4a00a982e9ccc7e0e51f0a68f62
Dan Morriss <danielmorriss1@gmail.com>
2025-07-30 21:52:59 +0100

    add styles to about section

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AboutPageComponent } from "../about-page/about-page.component";
+import { ContactPageComponent } from "../contact-page/contact-page.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, AboutPageComponent],
+  imports: [NavbarComponent, AboutPageComponent, ContactPageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
